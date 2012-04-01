11484c8ad570e043ee9e0a2fcddcfa3b036c5edc
Chris <chris@codinginertia.com>
2012-03-31 19:47:02 -0500

    Articles crud

---
+++ apwarming/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apwarming::Application.config.secret_token = '25146a04adc2d98400bd1e2f8969dcc5d611daba860c1eb6519b0a6eadf10ddf8e38b80f7a5c7bcc3eeea169ab221cf32f5d9a0c4188a4fda06688d059fe146d'
